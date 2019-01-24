dd091665efacfb26d5b58265bfce7af6fe44c63c
emb715 <yoelrola@gmail.com>
2019-01-24 20:25:35 -0300

    add load async images, improve load fonts with AppLoader

--- App.js
+++ App.js
@@ -5,11 +5,29 @@ import { attach } from 'proppy-react';
 import { compose, withState, withHandlers, withProps } from 'proppy';
 import selectorPlugin from '@rematch/select';
 import { ProppyProvider } from 'proppy-react';
-import { Font, AppLoading } from 'expo';
+import { Font, AppLoading, Asset } from 'expo';
 import models from './src/modules';
-import { NavigationService } from './src/services';
-
+import { NavigationService, ErrorHandlerService } from './src/services';
 import { router as Router } from './src/router';
+
+const fontsToLoad = {
+  'Rubik-Black': require('./node_modules/@shoutem/ui/fonts/Rubik-Black.ttf'),
+  'Rubik-BlackItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-BlackItalic.ttf'),
+  'Rubik-Bold': require('./node_modules/@shoutem/ui/fonts/Rubik-Bold.ttf'),
+  'Rubik-BoldItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-BoldItalic.ttf'),
+  'Rubik-Italic': require('./node_modules/@shoutem/ui/fonts/Rubik-Italic.ttf'),
+  'Rubik-Light': require('./node_modules/@shoutem/ui/fonts/Rubik-Light.ttf'),
+  'Rubik-LightItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-LightItalic.ttf'),
+  'Rubik-Medium': require('./node_modules/@shoutem/ui/fonts/Rubik-Medium.ttf'),
+  'Rubik-MediumItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-MediumItalic.ttf'),
+  'Rubik-Regular': require('./node_modules/@shoutem/ui/fonts/Rubik-Regular.ttf'),
+  'rubicon-icon-font': require('./node_modules/@shoutem/ui/fonts/rubicon-icon-font.ttf'),
+};
+
+const imagesToLoad = [
+  require('./assets/images/expo-icon.png'),
+  require('./assets/images/expo-splash.png'),
+];
 
 const select = selectorPlugin({ sliceState: rootState => rootState });
 const rematchInit = { models, plugins: [select] };
@@ -21,33 +39,33 @@ const providers = { getState, dispatch, store };
 const P = compose(
   withState('loading', 'setLoading', true),
   withHandlers({
-    loadFonts: ({ setLoading }) => async() => {
-      await Font.loadAsync({
-        'Rubik-Black': require('./node_modules/@shoutem/ui/fonts/Rubik-Black.ttf'),
-        'Rubik-BlackItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-BlackItalic.ttf'),
-        'Rubik-Bold': require('./node_modules/@shoutem/ui/fonts/Rubik-Bold.ttf'),
-        'Rubik-BoldItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-BoldItalic.ttf'),
-        'Rubik-Italic': require('./node_modules/@shoutem/ui/fonts/Rubik-Italic.ttf'),
-        'Rubik-Light': require('./node_modules/@shoutem/ui/fonts/Rubik-Light.ttf'),
-        'Rubik-LightItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-LightItalic.ttf'),
-        'Rubik-Medium': require('./node_modules/@shoutem/ui/fonts/Rubik-Medium.ttf'),
-        'Rubik-MediumItalic': require('./node_modules/@shoutem/ui/fonts/Rubik-MediumItalic.ttf'),
-        'Rubik-Regular': require('./node_modules/@shoutem/ui/fonts/Rubik-Regular.ttf'),
-        'rubicon-icon-font': require('./node_modules/@shoutem/ui/fonts/rubicon-icon-font.ttf'),
+    loadImages: () => async() => {
+      const cacheImages = imagesToLoad.map((image) => {
+        return Asset.fromModule(image).downloadAsync();
       });
-      setLoading(false);
+      return Promise.all(cacheImages)
+    },
+    loadAssets: () => async() => {
+      await Font.loadAsync(fontsToLoad);
+    },
+    loadAssetsFail: () => () => {
+      ErrorHandlerService.error('loadAssetsFail > error al cargar los archivos');
     }
   }),
-  withProps(({ loadFonts }) => {
-    loadFonts();
+  withProps(({ loadImages }) => {
+    // Load images to cache async
+    loadImages();
   }),
-  
 );
 
-const App = ({ loading }) => {
+const App = ({ loading, loadAssets, setLoading, loadAssetsFail }) => {
   if (loading) {
     return (
-      <AppLoading />
+      <AppLoading
+        startAsync={loadAssets}
+        onFinish={() => setLoading(false)}
+        onError={loadAssetsFail}
+      />
     );
   }
   return (
