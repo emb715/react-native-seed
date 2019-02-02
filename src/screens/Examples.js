b0ae443d2b9494c7c1eef9da6197e43d29987205
emb715 <yoelrola@gmail.com>
2019-02-02 15:32:21 -0300

    add screen component, header props,  add title for color palette

--- src/screens/Examples.js
+++ src/screens/Examples.js
@@ -1,48 +1,46 @@
 import React from 'react';
 import { compose, withHandlers, withProps, didSubscribe } from 'proppy';
 // import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { attach } from 'proppy-react';
 import { View, Text } from 'react-native';
-import { Button, Header } from 'react-native-elements';
 import { NavigationService } from '../services';
+import { Screen } from '../components';
 import ColorPalette from './examples/colorPalette';
 
 const P = compose(
   withHandlers({
-    goTo: () => () => {
+    headerRightOnPress: () => () => {
       NavigationService.navigate({ routeName: 'Main' });
     },
   }),
-  withProps(),
+  withProps(({ headerRightOnPress }) => ({
+    headerProps: {
+      title: 'Examples',
+      rightProps: {
+        icon: 'home',
+        color: '#fff',
+        onPress: headerRightOnPress,
+      }
+    }
+  })),
   didSubscribe((props) => {
     console.log('ExamplesScreen MOUNT PROPS', props);
   }),
 );
-const ExamplesScreen = ({ goTo, userData }) => {
-  console.log('ExamplesScreen RENDER', userData);
+const ExamplesScreen = ({ headerProps }) => {
+  console.log('ExamplesScreen RENDER');
   return (
-    <View>
-      <Header
-        leftComponent={{ icon: 'menu', color: '#fff' }}
-        centerComponent={{ text: 'Examples', style: { color: '#fff' } }}
-        rightComponent={{ icon: 'home', color: '#fff' }}
-      />
-      <Button
-        title="Main Screen"
-        onPress={goTo}
-      >
-        <Text>Main Screen</Text>
-      </Button>
-      <ColorPalette />
-    </View>
+    <Screen withContent headerProps={headerProps}>
+      <View>
+        <Text>Color Palette</Text>
+        <ColorPalette />
+      </View>
+    </Screen>
   );
 };
 
 ExamplesScreen.propTypes = {};
 
-ExamplesScreen.defaultProps = {
-  
-};
+ExamplesScreen.defaultProps = {};
 
 export default attach(P)(ExamplesScreen);
