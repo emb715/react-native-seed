41a4f0bd8e01361e87f3bcd396871b55eac9f08a
emb715 <yoelrola@gmail.com>
2019-02-02 15:34:20 -0300

    add colors and units

--- src/theme/variables.js
+++ src/theme/variables.js
@@ -11,19 +11,37 @@ const unitDp = 16;
 const unitPx = getPixelSizeForLayoutSize(unitDp);
 
 const gap = {
+  xxs: unitDp / 8, 
   xs: unitDp / 4,
   sm: unitDp / 2,
   md: unitDp,
   lg: unitDp * 1.5,
   xl: unitDp * 2,
+  xxl: unitDp * 4,
 };
 
 const colors = {
-  primary: '',
-  secundary: '',
-  tertiary: '',
-  accent: '',
-  accentVariant: '',
+  primary: {
+    100: '#FF7A80',
+    300: '#FF545C',
+    500: '#f51832',
+    700: '#D1262E',
+    900: '#A31E24',
+  },
+  secundary: {
+    100: '#A877F1',
+    300: '#8F50ED',
+    500: '#772AEA',
+    700: '#6223C0',
+    900: '#4C1B95',
+  },
+  accent: {
+    100: '#EE75B7',
+    300: '#E94EA3',
+    500: '#E5278F',
+    700: '#BC2076',
+    900: '#92195C',
+  },
   neutral: {
     100: '#f9f9f9',
     300: '#e1e7ec',
@@ -35,10 +53,21 @@ const colors = {
   },
 };
 
+const input = {
+  height: 44,
+};
+
+const font = {
+  // family: '',
+  regular: unitDp,
+
+};
+
 export default {
   deviceWidth,
   deviceHeight,
   containerHeight: Platform.OS === 'ios' ? deviceHeight : deviceHeight - 20,
   gap,
   colors,
+  size: gap,
 };
